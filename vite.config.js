12de5b3e97c4b950b255bb5470bf625821fcad98
Liang_Wang <liangwang.fr@gmail.com>
2024-09-01 14:48:00 +0200

    udpate

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/facebook/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        friendBlog: 'friend-blog.html'
+      }
+    }
+  },
   plugins: [react()],
 })
